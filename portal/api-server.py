ef9f2a3ac4f2e8ed74268d884af9700dc9ce2e97
geumseongYoon <gsyoon@smartx.kr>
2019-12-02 01:16:32 +0000

    code test2

--- portal/api-server.py
+++ portal/api-server.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-  #cmd = "kubectl get nodes"
-  #out = subprocess.check_output([cmd], shell=True)
+  cmd = "kubectl get nodes"
+  out = subprocess.check_output([cmd], shell=True)
   #print(out)
   #print(type(out))
-  return 'hello world'
+  return out
 
 if __name__ == '__main__':
   app.run(host="203.237.53.192", port=5001)
